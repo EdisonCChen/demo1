bcd8141836ed4334455550544b9c909726fa4d35
CHENJH <609344989@qq.com>
2018-11-08 17:34:59 +0800

    webservice + 生成图片 webp

--- src/main/java/com/refreshPlatemain.java
+++ src/main/java/com/refreshPlatemain.java
@@ -12,25 +12,15 @@ public class refreshPlatemain {
     public static void main(String[] args) {
         List<Userinfo> list = new ArrayList<Userinfo>();
         Userinfo userinfo = new Userinfo();
-        userinfo.setNodeIp("192.168.60.117");
-        userinfo.setCompany("茜莓科技");
-        userinfo.setUserName("张三啊啊啊啊");
-        userinfo.setUserRank("主任");
+        userinfo.setNodeIp("1");
+        userinfo.setElementNo2("席媒科技");
+        userinfo.setElementNo1("柯麦德");
+        userinfo.setElementNo3("常委会主任");
+        userinfo.setElementNo4("第四");
+        userinfo.setElementNo5("第五");
         list.add(userinfo);
-        Userinfo userinfo1 = new Userinfo();
-        userinfo1.setNodeIp("192.168.60.1");
-        userinfo1.setCompany("茜莓科技");
-        userinfo1.setUserName("李四");
-        userinfo1.setUserRank("主任");
-        list.add(userinfo1);
-        Userinfo userinfo2 = new Userinfo();
-        userinfo2.setNodeIp("192.168.60.2");
-        userinfo2.setCompany("茜莓科技");
-//        userinfo2.setUserName("");
-        userinfo2.setUserRank("主任");
-        list.add(userinfo2);
         try {
-            String backmsg=LayoutTemplateServiceImpl.createLayoutImage(list);
+            String backmsg=LayoutTemplateServiceImpl.createLayoutImage(list,"D:\\Users\\CHENJH\\Desktop\\LayoutConfig.json");
             System.out.println(backmsg);
         } catch (Exception e) {
             e.printStackTrace();
